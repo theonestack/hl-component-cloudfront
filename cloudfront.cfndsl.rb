510db18e29c2f5cd91723fb6116a8ac6971c2d5c
Aaron Walker <a.walker@base2services.com>
2019-03-05 13:57:26 +0100

    adds support for adding a CNAME record for the cloudfront dist (#4)
    * adds support for adding a CNAME record for the cloudfront dist
    
    * support adding an origin path

--- cloudfront.cfndsl.rb
+++ cloudfront.cfndsl.rb
@@ -15,26 +15,21 @@ CloudFormation do
       Id: id,
       DomainName: Ref("#{id}OriginDomainName")
     }
-
+    origin[:OriginPath] = config['origin_path'] if config.has_key?('origin_path')
     case config['source']
-    when 'loadbalancer'
-
+    when 'loadbalancer', 'apigateway'
       origin[:CustomOriginConfig] = { HTTPPort: '80', HTTPSPort: '443' }
       origin[:CustomOriginConfig][:OriginKeepaliveTimeout] = config["keep_alive_timeout"] if config.has_key?('keep_alive_timeout')
       origin[:CustomOriginConfig][:OriginReadTimeout] = config["read_timeout"] if config.has_key?('read_timeout')
       origin[:CustomOriginConfig][:OriginSSLProtocols] = config['ssl_policy'] if config.has_key?('ssl_policy')
       origin[:CustomOriginConfig][:OriginProtocolPolicy] = config['protocol_policy']
-
     when 's3'
-
       CloudFront_CloudFrontOriginAccessIdentity("#{id}OriginAccessIdentity") {
         CloudFrontOriginAccessIdentityConfig({
           Comment: FnSub("${EnvironmentName}-#{id}-CloudFrontOriginAccessIdentity")
         })
       }
       origin[:S3OriginConfig] = { OriginAccessIdentity: FnSub("origin-access-identity/cloudfront/${#{id}OriginAccessIdentity}") }
-
-
     end
 
     distribution_config[:Origins] << origin
@@ -86,5 +81,20 @@ CloudFormation do
     DistributionConfig distribution_config
     Tags tags
   }
+
+  dns_records.each_with_index do |dns, index|
+    Route53_RecordSet("CloudfrontDns#{index}") do
+      HostedZoneName FnSub('${DnsDomain}.')
+      Name FnSub("#{dns}")
+      Type 'CNAME'
+      TTL '60'
+      ResourceRecords [FnGetAtt('Distribution', 'DomainName')]
+    end
+  end if defined? dns_records
+
+  Output('DomainName') do
+    Value(FnGetAtt('Distribution', 'DomainName'))
+    Export FnSub("${EnvironmentName}-#{component_name}-DomainName")
+  end
 
 end
